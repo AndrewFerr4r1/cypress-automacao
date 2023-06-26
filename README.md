a60ff707d7f47e5bfa00a5166f960d1664c41496
Andrew Ferrari <andrew.ferrari@sptech.school>
2023-06-26 20:10:55 -0300

    config projeto e adicionando o primeiro caso de teste

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cypress-automacao
+# automação usando cypress
+
+<h3>Site usado para realizar a automação</h3><br>
+<p>https://devfinance-agilizei.netlify.app/#p</p> 
---
+++ cypress/e2e/finances.cy.js
@@ -0,0 +1,15 @@
+
+
+describe('Transações', () => {
+    it('Cadastrar uma entrada', () => {
+        cy.visit("https://devfinance-agilizei.netlify.app/#")
+
+        cy.contains("Nova Transação").click();
+        cy.get('#description').type("Freela");
+        cy.get('#amount').type(250);
+        cy.get('#date').type("2023-02-15"); // yyyy-mm-dd
+
+        cy.contains('button', 'Salvar').click();
+
+    });
+});
