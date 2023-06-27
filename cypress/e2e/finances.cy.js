29aaefda748d47e9eecc00dd798d40d7b4a565b7
Andrew Ferrari <andrew.ferrari@sptech.school>
2023-06-27 17:06:19 -0300

    Incrementando alguns testes

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,15 +1,47 @@
 
 
 describe('Transações', () => {
-    it('Cadastrar uma entrada', () => {
+
+    beforeEach(() => {
         cy.visit("https://devfinance-agilizei.netlify.app/#")
+    });
 
-        cy.contains("Nova Transação").click();
-        cy.get('#description').type("Freela");
-        cy.get('#amount').type(250);
-        cy.get('#date').type("2023-02-15"); // yyyy-mm-dd
+    it('Cadastrar uma entrada', () => {
+        criarTransacao("Freela", 250)
 
-        cy.contains('button', 'Salvar').click();
+        cy.get("tbody tr td.description").should("have.text", "Freela")
 
     });
+
+    it('Cadastrar uma saida', () => {
+        criarTransacao("Cinema", -45)
+
+        cy.get("tbody tr td.description").should("have.text", "Cinema")
+
+    });
+
+    it('Excluir trasação', () => {
+        criarTransacao("Freela", 100)
+        criarTransacao("Mesada", 10)
+
+        cy.contains(".description", "Freela")
+            .parent()
+            .find('img')
+            .click()
+
+        cy.get('tbody tr').should("have.length", 1)
+    });
+
+
 });
+
+function criarTransacao(descricao, valor) {
+
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type(descricao)
+    cy.get('#amount').type(valor)
+    cy.get('#date').type("2023-02-15") // yyyy-mm-dd
+
+    cy.contains('button', 'Salvar').click()
+        
+}
